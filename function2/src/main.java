1ffdd9980cf0de4135080ebc9944aad858de86d0
Hyeongwon-up <leehwdatabase@gmail.com>
2021-01-20 08:04:39 +0900

    210119

---
+++ function2/src/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static char function(String input) {
+        return input.charAt(input.length()-1);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
