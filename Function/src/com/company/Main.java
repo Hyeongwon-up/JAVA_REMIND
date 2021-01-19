ae6552edb218deb47d670f543cd3a31dfbe944bd
Hyeongwon-up <leehwdatabase@gmail.com>
2021-01-19 21:38:28 +0900

    210119

---
+++ Function/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static int function(int a, int b, int c) {
+        int min;
+        if(a>b) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
